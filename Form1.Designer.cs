3508bce876d7b2bd1b56afc0e2e48dca6cd778da
antoniosetya <antonio_setya@hotmail.com>
2018-03-16 22:57:37 +0700

    Migrating from WPF to normal Windows app
    Also :
    * Graph drawing is available now
    * Adding comments
    By migrating, the required stack size is also reduced! Yay!

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LectureScheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FileContent = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // FileContent
+            // 
+            this.FileContent.Location = new System.Drawing.Point(12, 62);
+            this.FileContent.Name = "FileContent";
+            this.FileContent.Size = new System.Drawing.Size(387, 213);
+            this.FileContent.TabIndex = 2;
+            this.FileContent.Text = "Edges will be displayed here...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 450);
+            this.Controls.Add(this.FileContent);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Form GraphWindow = new System.Windows.Forms.Form();
+        private System.Windows.Forms.RichTextBox FileContent;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LectureScheduler
+{
+    public partial class Form1 : Form
+    {
+        public List<Edge> EdgeList = new List<Edge>(); // Edge list
+        public List<string> NodeList = new List<string>(); // Node list
+        Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer(); // Graph viewer engine
+        Microsoft.Msagl.Drawing.Graph graph; // The graph that MSAGL accepts
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".txt";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show file dialog
+            if (result == DialogResult.OK) // If the file dialog retrieves a file
+            {
+                graph = new Microsoft.Msagl.Drawing.Graph("graph"); // Initialize new MSAGL graph
+                FileContent.Text = ""; // Clear FileContent textbox
+                using (StreamReader sr = new StreamReader(openFileDialog1.OpenFile()))
+                {
+                    string line, target_node, preq_node;
+                    int i;
+                    Edge temp;
+                    while (sr.Peek() >= 0)
+                    {
+                        line = sr.ReadLine();
+                        target_node = "";
+                        i = 0;
+                        // Get current lecture
+                        while ((i < line.Length) && (line[i] != ',') && (line[i] != '.'))
+                        {
+                            target_node += line[i];
+                            i++;
+                        }
+                        if (line[i] != '.') i++;
+                        // Add node to MSAGL graph (first line) and to the node list (second line)
+                        graph.AddNode(target_node);
+                        NodeList.Add(string.Copy(target_node));
+                        // Get prerequisite lecture
+                        while ((i < line.Length) && (line[i] != '.'))
+                        {
+                            preq_node = "";
+                            while ((i < line.Length) && (line[i] != ',') && (line[i] != '.'))
+                            {
+                                preq_node += line[i];
+                                i++;
+                            }
+                            if (line[i] != '.') i++;
+                            // Add this edge to MSAGL graph (first line) and to the edge list (remaining)
+                            graph.AddEdge(preq_node, target_node);
+                            temp = new Edge(preq_node, target_node);
+                            EdgeList.Add(temp);
+                        }
+                    }
+                }
+                foreach (Edge element in EdgeList)
+                {
+                    FileContent.Text += element.FormatOutput();
+                    FileContent.Text += "\n";
+                }
+                // Bind graph to viewer engine
+                viewer.Graph = graph;
+                // Bind viewer engine to second window
+                GraphWindow.SuspendLayout();
+                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                GraphWindow.Controls.Add(viewer);
+                GraphWindow.ResumeLayout();
+                // Shows second window
+                GraphWindow.ShowDialog();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+
+    public class Edge
+    {
+        // Nodes that are connected by this edge
+        public string N1;
+        public string N2;
+        // ctor
+        public Edge(string _n1 = "", string _n2 = "")
+        {
+            N1 = _n1;
+            N2 = _n2;
+        }
+        // Formats output
+        public string FormatOutput()
+        {
+            return ("<" + N1 + "," + N2 + ">");
+        }
+    }
+}
