18f3e5776f5cb9120fe4d06d9699eef41cea6b93
antoniosetya <antonio_setya@hotmail.com>
2018-03-18 09:52:12 +0700

    Changing data structure

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,11 @@ namespace LectureScheduler
 {
     public partial class Form1 : Form
     {
-        public List<Edge> EdgeList = new List<Edge>(); // Edge list
-        public List<string> NodeList = new List<string>(); // Node list
+        List<Courses> Course = new List<Courses>();
+
         Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer(); // Graph viewer engine
         Microsoft.Msagl.Drawing.Graph graph; // The graph that MSAGL accepts
+
         public Form1()
         {
             InitializeComponent();
@@ -32,46 +33,32 @@ namespace LectureScheduler
                 FileContent.Text = ""; // Clear FileContent textbox
                 using (StreamReader sr = new StreamReader(openFileDialog1.OpenFile()))
                 {
-                    string line, target_node, preq_node;
-                    int i;
-                    Edge temp;
+                    string line;
+                    Courses temp;
+                    //Edge temp;
                     while (sr.Peek() >= 0)
                     {
                         line = sr.ReadLine();
-                        target_node = "";
-                        i = 0;
-                        // Get current lecture
-                        while ((i < line.Length) && (line[i] != ',') && (line[i] != '.'))
+                        line = line.Replace(".",string.Empty);
+                        line = line.Replace(","," ");
+                        string [] cur_line = line.Split(' ');
+                        temp = new Courses(cur_line[0]);
+                        Course.Add(temp);
+                        graph.AddNode(cur_line[0]);
+                        for (int i = 1; i < cur_line.Length; i++)
                         {
-                            target_node += line[i];
-                            i++;
-                        }
-                        if (line[i] != '.') i++;
-                        // Add node to MSAGL graph (first line) and to the node list (second line)
-                        graph.AddNode(target_node);
-                        NodeList.Add(string.Copy(target_node));
-                        // Get prerequisite lecture
-                        while ((i < line.Length) && (line[i] != '.'))
-                        {
-                            preq_node = "";
-                            while ((i < line.Length) && (line[i] != ',') && (line[i] != '.'))
-                            {
-                                preq_node += line[i];
-                                i++;
-                            }
-                            if (line[i] != '.') i++;
-                            // Add this edge to MSAGL graph (first line) and to the edge list (remaining)
-                            graph.AddEdge(preq_node, target_node);
-                            temp = new Edge(preq_node, target_node);
-                            EdgeList.Add(temp);
+                            Course[Course.Count-1].addPreReqCourses(cur_line[i]);
+                            graph.AddEdge(cur_line[i],cur_line[0]);
                         }
                     }
                 }
-                foreach (Edge element in EdgeList)
+                System.Console.WriteLine(Course.Count);
+                foreach (Courses element in Course)
                 {
-                    FileContent.Text += element.FormatOutput();
-                    FileContent.Text += "\n";
+                    FileContent.Text = FileContent.Text + element.FormatOutput();
+                    FileContent.Text = FileContent.Text + "\n";
                 }
+
                 // Bind graph to viewer engine
                 viewer.Graph = graph;
                 // Bind viewer engine to second window
@@ -105,6 +92,75 @@ namespace LectureScheduler
         public string FormatOutput()
         {
             return ("<" + N1 + "," + N2 + ">");
+        }
+    }
+
+    public class Courses
+    {
+        private string course;
+        private List<string> preReqCourses = new List<string>();
+        private int numberOfPreRequisite;
+
+        // ctor
+        public Courses(string _course = "")
+        {
+            course = _course;
+            numberOfPreRequisite = 0;
+        }
+        //Setter
+        public void setCourse(string course)
+        {
+            this.course = course;
+        }
+        public void addPreReqCourses(string courses)
+        {
+            preReqCourses.Add(courses);
+            numberOfPreRequisite++;
+        }
+        public void setNumberOfPreRequisite(int n)
+        {
+            this.numberOfPreRequisite = n;
+        }
+        //Getter
+        public string getCourses()
+        {
+            return course;
+        }
+        public int getNumberOfPreRequisite()
+        {
+            return numberOfPreRequisite;
+        }
+        public bool isNoPreRequisite()
+        {
+            return numberOfPreRequisite == 0;
+        }
+        public bool isPreRequisite(string s)
+        {
+            bool found = false;
+            int i = 0;
+            while ((!found) && (i < preReqCourses.Count))
+            {
+                if (s == preReqCourses[i])
+                {
+                    found = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return found;
+        }
+        // Member function
+        // Format output
+        public string FormatOutput()
+        {
+            string str_out = "";
+            foreach(string preq in preReqCourses)
+            {
+                str_out += "<" + preq + "," + course + ">";
+            }
+            return str_out;
         }
     }
 }
